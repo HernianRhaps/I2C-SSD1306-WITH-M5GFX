b1374bfebcc5d4c3bbc18e86009303bea12607bf
SkatanMustang <96286652+SkatanMustang@users.noreply.github.com>
2023-07-12 00:34:17 +0900

    Update M5_GENERIC_SSD1306.h

--- M5_GENERIC_SSD1306.h
+++ M5_GENERIC_SSD1306.h
@@ -14,7 +14,7 @@
 #define GENERIC_SSD1306_ADDR 0x3C
 #define GENERIC_SSD1306_FREQ 400000
 
-class M5_GENERIC_SSD1306  : public M5GFX
+class SSD1306_WITH_M5GFX  : public M5GFX
 {
   lgfx::Bus_I2C::config_t _bus_cfg;
 
@@ -32,14 +32,14 @@ public:
 
   config_t config(void) const { return config_t(); }
 
-  M5_GENERIC_SSD1306 (const config_t &cfg)
+  SSD1306_WITH_M5GFX (const config_t &cfg)
   {
     uint8_t pin_sda = cfg.pin_sda < GPIO_NUM_MAX ? cfg.pin_sda : GENERIC_SSD1306_SDA;
     uint8_t pin_scl = cfg.pin_scl < GPIO_NUM_MAX ? cfg.pin_scl : GENERIC_SSD1306_SCL;
     setup(pin_sda, pin_scl, cfg.i2c_freq, cfg.i2c_port, cfg.i2c_addr);
   }
 
-  M5_GENERIC_SSD1306 (uint8_t pin_sda = GENERIC_SSD1306_SDA, uint8_t pin_scl = GENERIC_SSD1306_SCL, uint32_t i2c_freq = GENERIC_SSD1306_FREQ, int8_t i2c_port = -1, uint8_t i2c_addr = GENERIC_SSD1306_ADDR)
+  SSD1306_WITH_M5GFX (uint8_t pin_sda = GENERIC_SSD1306_SDA, uint8_t pin_scl = GENERIC_SSD1306_SCL, uint32_t i2c_freq = GENERIC_SSD1306_FREQ, int8_t i2c_port = -1, uint8_t i2c_addr = GENERIC_SSD1306_ADDR)
   {
     setup(pin_sda, pin_scl, i2c_freq, i2c_port, i2c_addr);
     
@@ -48,10 +48,10 @@ public:
   using lgfx::LGFX_Device::init;
   bool init(uint8_t pin_sda, uint8_t pin_scl, uint32_t i2c_freq = GENERIC_SSD1306_FREQ, int8_t i2c_port = -1, uint8_t i2c_addr = GENERIC_SSD1306_ADDR)
   {
+
     setup(pin_sda, pin_scl, i2c_freq, i2c_port, i2c_addr);
     return init();
   }
-
 
   void setup(uint8_t pin_sda = GENERIC_SSD1306_SDA, uint8_t pin_scl = GENERIC_SSD1306_SCL, uint32_t i2c_freq = GENERIC_SSD1306_FREQ, int8_t i2c_port = -1, uint8_t i2c_addr = GENERIC_SSD1306_ADDR)
   {
@@ -92,19 +92,17 @@ public:
       return true;
     }
 
+
     auto p = new lgfx::Panel_SSD1306();
     auto b = new lgfx::Bus_I2C();
     b->config(_bus_cfg);
 
     {
-
       p->bus(b);
       auto cfg = p->config();
       cfg.offset_rotation = 2;
       p->config(cfg);
     }
-
-
 
     setPanel(p);
 
@@ -113,7 +111,6 @@ public:
       _panel_last.reset(p);
       _bus_last.reset(b);
 
-
       return true;
     }
 
@@ -121,6 +118,7 @@ public:
 
     delete p;
     delete b;
+
     return false;
 
   }
@@ -151,3 +149,5 @@ public:
 */
 
 };
+
+
