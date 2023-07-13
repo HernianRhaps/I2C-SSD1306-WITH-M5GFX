b91ac7f01d41bdd354d1af8f47122320032d9783
SkatanMustang <96286652+SkatanMustang@users.noreply.github.com>
2023-07-13 10:23:31 +0900

    Add files via upload

--- example/SSD1306withM5GFX_avatar_test.ino
+++ example/SSD1306withM5GFX_avatar_test.ino
@@ -12,12 +12,36 @@ void setup(void){
 
   Serial.begin(115200);
 
-  display.init();
+  auto board=M5.getBoard();
+  Serial.print("M5.getBoard()");
+ 
+  if(lgfx::boards::board_M5Atom==board)
+    Serial.println("board_M5Atom");
+  else if(lgfx::boards::board_unknown==board)//ESP32 DevKitC is recognized as board_unknown
+    Serial.println("board_unknown");
 
+  display.init();// it works.it can display contents of setup().And it can't display avatar
+
+  Serial.print("M5.getDisplayCount()");
+  Serial.println(M5.getDisplayCount());
+
+  Serial.print("M5.addDisplay(display)");
+  Serial.println(M5.addDisplay(display));
+
+  Serial.print("M5.getDisplayCount()");
+  Serial.println(M5.getDisplayCount());
+
+  //display.init();// it can display contents of setup().but it can't display avatar.I don'nt know why
+
+  //M5.Display.init();// M5.Display.init() doesn't work.
+  //M5.Lcd.init();// M5.Lcd.init() doesn't work.
+
+
+  // count down (check display.init())
+  display.setRotation(2);
   display.setTextSize(1);
   display.drawString("Test execute", 0, 10);
   display.drawString("Wait 10sec", 0, 25);
-
   delay(1000);
 
   display.setTextSize(2);
@@ -30,32 +54,12 @@ void setup(void){
   }
 
 
-  auto board=M5.getBoard();
-  Serial.print("M5.getBoard()");
- 
-  if(lgfx::boards::board_M5Atom==board)
-    Serial.println("board_M5Atom");
-  else if(lgfx::boards::board_unknown==board)
-    Serial.println("board_unknown");
-
-  Serial.print("M5.getDisplayCount()");
-  Serial.println(M5.getDisplayCount());
-
-  Serial.print("M5.addDisplay(display)");
-  Serial.println(M5.addDisplay(display));
-
-  Serial.print("M5.getDisplayCount()");
-  Serial.println(M5.getDisplayCount());
-
   //delay(10000);
 
   // avatar
   avatar.setScale(0.35);
   avatar.setPosition(0, 25);
   avatar.init();  // start drawing
-
-  //M5.Display.init();// M5.Display.init() doesn't work.
-
 } 
 
 
